b6470bb03ed6fb218826ef8e92639e535180aad5
Tobias Schlatter <tobias@meisch.ch>
2014-06-13 10:42:28 +0200

    Step 3: Using the DOM

--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -1,7 +1,16 @@
 package tutorial.webapp
 
+import org.scalajs.dom
+import org.scalajs.dom.document
+
 object TutorialApp {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    appendPar(document.body, "Hello World")
+  }
+
+  def appendPar(targetNode: dom.Node, text: String): Unit = {
+    val parNode = document.createElement("p")
+    parNode.textContent = text
+    targetNode.appendChild(parNode)
   }
 }
