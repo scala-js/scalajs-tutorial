57fac5c5165a4c53ff2f4824190e596a41d9ffe8
Tobias Schlatter <tobias@meisch.ch>
2014-06-13 10:42:28 +0200

    Step 3: Using the DOM

--- build.sbt
+++ build.sbt
@@ -5,3 +5,5 @@ scalaVersion := "2.13.1"
 
 // This is an application with a main method
 scalaJSUseMainModuleInitializer := true
+
+libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "1.0.0"
--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -1,7 +1,17 @@
 package tutorial.webapp
 
+import org.scalajs.dom
+import org.scalajs.dom.document
+
 object TutorialApp {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    appendPar(document.body, "Hello World")
+  }
+
+  def appendPar(targetNode: dom.Node, text: String): Unit = {
+    val parNode = document.createElement("p")
+    val textNode = document.createTextNode(text)
+    parNode.appendChild(textNode)
+    targetNode.appendChild(parNode)
   }
 }
