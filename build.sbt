bda00b3e4846dd53da0bacea9a3c84764e891d82
Tobias Schlatter <tobias@meisch.ch>
2014-06-14 16:18:27 +0200

    Step 6: Testing

--- build.sbt
+++ build.sbt
@@ -7,3 +7,10 @@ scalaVersion := "2.13.6"
 scalaJSUseMainModuleInitializer := true
 
 libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "1.2.0"
+
+// Add support for the DOM in `run` and `test`
+jsEnv := new org.scalajs.jsenv.jsdomnodejs.JSDOMNodeJSEnv()
+
+// uTest settings
+libraryDependencies += "com.lihaoyi" %%% "utest" % "0.7.9" % "test"
+testFrameworks += new TestFramework("utest.runner.Framework")
---
+++ src/test/scala/tutorial/webapp/TutorialTest.scala
@@ -0,0 +1,35 @@
+package tutorial.webapp
+
+import utest._
+
+import scala.scalajs.js
+
+import org.scalajs.dom
+import org.scalajs.dom.document
+import org.scalajs.dom.ext._
+
+object TutorialTest extends TestSuite {
+
+  // Initialize App
+  TutorialApp.setupUI()
+
+  def tests = Tests {
+    test("HelloWorld") {
+      assert(document.querySelectorAll("p").count(_.textContent == "Hello World") == 1)
+    }
+
+    test("ButtonClick") {
+      def messageCount =
+        document.querySelectorAll("p").count(_.textContent == "You clicked the button!")
+
+      val button = document.querySelector("button").asInstanceOf[dom.html.Button]
+      assert(button != null && button.textContent == "Click me!")
+      assert(messageCount == 0)
+
+      for (c <- 1 to 5) {
+        button.click()
+        assert(messageCount == c)
+      }
+    }
+  }
+}
