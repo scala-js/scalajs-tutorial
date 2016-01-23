0296d63024ecaee5748262bc169097bb99b483d3
Sébastien Doeraene <sjrdoeraene@gmail.com>
2016-01-23 11:10:26 +0100

    Step 5: Setup the UI in Scala.js

--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -1,12 +1,23 @@
 package tutorial.webapp
 
-import scala.scalajs.js.annotation.JSExportTopLevel
-
 import org.scalajs.dom
 import org.scalajs.dom.document
 
 object TutorialApp {
   def main(args: Array[String]): Unit = {
+    document.addEventListener("DOMContentLoaded", { (e: dom.Event) =>
+      setupUI()
+    })
+  }
+
+  def setupUI(): Unit = {
+    val button = document.createElement("button")
+    button.textContent = "Click me!"
+    button.addEventListener("click", { (e: dom.MouseEvent) =>
+      addClickedMessage()
+    })
+    document.body.appendChild(button)
+
     appendPar(document.body, "Hello World")
   }
 
@@ -16,7 +27,6 @@ object TutorialApp {
     targetNode.appendChild(parNode)
   }
 
-  @JSExportTopLevel("addClickedMessage")
   def addClickedMessage(): Unit = {
     appendPar(document.body, "You clicked the button!")
   }
