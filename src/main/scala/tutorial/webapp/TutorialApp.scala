2cd078d8843f599dc2c653c8bfdd2e47e874df42
Sébastien Doeraene <sjrdoeraene@gmail.com>
2016-01-23 11:10:26 +0100

    Step 5: Using jQuery

--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -1,24 +1,18 @@
 package tutorial.webapp
 
-import scala.scalajs.js.annotation.JSExportTopLevel
-
-import org.scalajs.dom
-import dom.document
+import org.querki.jquery._
 
 object TutorialApp {
   def main(args: Array[String]): Unit = {
-    appendPar(document.body, "Hello World")
+    $(() => setupUI())
   }
 
-  def appendPar(targetNode: dom.Node, text: String): Unit = {
-    val parNode = document.createElement("p")
-    val textNode = document.createTextNode(text)
-    parNode.appendChild(textNode)
-    targetNode.appendChild(parNode)
+  def setupUI(): Unit = {
+    $("body").append("<p>Hello World</p>")
+    $("#click-me-button").click(() => addClickedMessage())
   }
 
-  @JSExportTopLevel("addClickedMessage")
   def addClickedMessage(): Unit = {
-    appendPar(document.body, "You clicked the button!")
+    $("body").append("<p>You clicked the button!</p>")
   }
 }
